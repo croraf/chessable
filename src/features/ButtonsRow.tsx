f819f54a758baa63a46de1ca8076d00e4533602f
croraf <vrafaeli@msn.com>
2020-10-19 00:18:59 +0200

    Minor Comment

--- src/features/ButtonsRow.tsx
+++ src/features/ButtonsRow.tsx
@@ -1,34 +1,25 @@
-import React from 'react';
-import { Button, } from '@material-ui/core';
+import React from "react";
+import { Button } from "@material-ui/core";
 
+// Not really fits in its own feature,
+// but put here as an example of a second feature
+// Should be inside Home.tsx I guess, or in the "chess" feature
 const ButtonsRow = ({
   rotateButtonClick,
   spawnPawnButtonClick,
-  resetButtonClick,
+  resetButtonClick
 }: {
-  rotateButtonClick: () => void,
-  spawnPawnButtonClick: () => void,
-  resetButtonClick: () => void,
+  rotateButtonClick: () => void;
+  spawnPawnButtonClick: () => void;
+  resetButtonClick: () => void;
 }) => {
-
   return (
     <div>
-      <Button
-        onClick={rotateButtonClick}
-      >
-        Rotate
-      </Button>
-      <Button
-        onClick={spawnPawnButtonClick}
-        style={{ margin: '1rem', }}
-      >
+      <Button onClick={rotateButtonClick}>Rotate</Button>
+      <Button onClick={spawnPawnButtonClick} style={{ margin: "1rem" }}>
         Spawn a white pawn
       </Button>
-      <Button
-        onClick={resetButtonClick}
-      >
-        Reset
-      </Button>
+      <Button onClick={resetButtonClick}>Reset</Button>
     </div>
   );
 };
